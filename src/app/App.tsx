1d71fb97e93ae139e1828392e0eeee4d92d3dde5
Shawn Chen <ethanloatrchen@gmail.com>
2021-05-04 13:01:45 -0700

    Using hash route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import { HashRouter, Switch, Route } from 'react-router-dom';
 import './App.scss';
 import Room from './room/Room';
 import Remote from './remote/remote';
@@ -11,16 +7,16 @@ import RemoteSearch from './remote/remote-search';
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Switch>
         <Route exact path="/">
           Homepage of ktv
         </Route>
-        <Route path="/room" component={Room}/>
+        <Route path="/room" component={Room} />
         <Route path="/remote" exact component={Remote} />
         <Route path="/remote/search" component={RemoteSearch} />
       </Switch>
-    </Router>
+    </HashRouter>
   );
 }
 
